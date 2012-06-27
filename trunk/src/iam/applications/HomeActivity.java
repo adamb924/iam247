befeb79926eb70c9b117a84b4438fd236bc639e8
adamb924@gmail.com <adamb924@gmail.com@48303951-de3c-2ae9-f8b3-d1ee7001000d>
2012-06-27 16:12:13 +0000

    Moved log and database operations to the PreferencesActivity interface.

--- trunk/src/iam/applications/HomeActivity.java
+++ trunk/src/iam/applications/HomeActivity.java
@@ -3,24 +3,14 @@
  */
 package iam.applications;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
 import java.util.Date;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -187,31 +177,12 @@ public class HomeActivity extends Activity {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.home_menu, menu);
 
-		// MenuItem thisIsAllowed = menu.findItem(R.id.thisis_enabled);
-		// thisIsAllowed.setCheckable(true);
-		// thisIsAllowed.setChecked(getThisIsAllowed(this));
-
 		return r;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.email_database:
-			emailDatabase();
-			return true;
-		case R.id.upload_database:
-			uploadDatabase();
-			return true;
-		case R.id.reset_database:
-			resetDatabase();
-			return true;
-		case R.id.clear_data:
-			clearDataBeforeOneWeek();
-			return true;
-		case R.id.clear_log:
-			clearLogBeforeOneWeek();
-			return true;
 		case R.id.customization:
 			startActivity(new Intent(this, PreferencesActivity.class));
 			return true;
@@ -221,125 +192,5 @@ public class HomeActivity extends Activity {
 		default:
 			return super.onOptionsItemSelected(item);
 		}
-	}
-
-	private void emailDatabase() {
-		FileChannel source = null;
-		FileChannel destination = null;
-
-		Log.i("Debug", mDbHelper.getPath());
-
-		final File sdcard = Environment.getExternalStorageDirectory();
-		File sourceFile = new File(
-				"/data/data/iam.applications/databases/thedatabase");
-		File destFile = new File(sdcard, "temp/thedatabase");
-
-		try {
-			source = new FileInputStream(sourceFile).getChannel();
-			destination = new FileOutputStream(destFile).getChannel();
-			destination.transferFrom(source, 0, source.size());
-		} catch (Exception e) {
-			try {
-				e.printStackTrace();
-
-				if (source != null) {
-					source.close();
-				}
-				if (destination != null) {
-					destination.close();
-				}
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		}
-
-		Intent sendIntent = new Intent(Intent.ACTION_SEND);
-		sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-		sendIntent.setType("application/x-sqlite3");
-		sendIntent.putExtra(Intent.EXTRA_SUBJECT, "24/7 App Database");
-		sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(destFile));
-		sendIntent.putExtra(Intent.EXTRA_TEXT,
-				"As of: " + Time.prettyDateTime(new Date()));
-		startActivity(Intent.createChooser(sendIntent, "Email:"));
-	}
-
-	private void uploadDatabase() {
-		AlertDialog.Builder alert = new AlertDialog.Builder(HomeActivity.this);
-		alert.setTitle(R.string.upload_database);
-		alert.setMessage(R.string.upload_database_instructions);
-		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int whichButton) {
-				FileChannel source = null;
-				FileChannel destination = null;
-
-				final File sdcard = Environment.getExternalStorageDirectory();
-				File sourceFile = new File(sdcard, "temp/thedatabase");
-				File destFile = new File(
-						"/data/data/iam.applications/databases/thedatabase");
-
-				try {
-					source = new FileInputStream(sourceFile).getChannel();
-					destination = new FileOutputStream(destFile).getChannel();
-					destination.transferFrom(source, 0, source.size());
-				} catch (Exception e) {
-					try {
-						e.printStackTrace();
-
-						if (source != null) {
-							source.close();
-						}
-						if (destination != null) {
-							destination.close();
-						}
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-				}
-
-				fillData();
-			}
-		});
-		alert.setNegativeButton("Cancel", null);
-		alert.show();
-	}
-
-	private void resetDatabase() {
-		AlertDialog.Builder alert = new AlertDialog.Builder(HomeActivity.this);
-		alert.setTitle(R.string.reset_database);
-		alert.setMessage(R.string.reset_database_first_warning);
-		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int whichButton) {
-
-				AlertDialog.Builder alert2 = new AlertDialog.Builder(
-						HomeActivity.this);
-				alert2.setTitle(R.string.reset_database);
-				alert2.setMessage(R.string.reset_database_second_warning);
-				alert2.setPositiveButton("Ok",
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int whichButton) {
-
-								mDbHelper.deleteAll();
-								fillData();
-
-							}
-						});
-				alert2.setNegativeButton("Cancel", null);
-				alert2.show();
-			}
-		});
-		alert.setNegativeButton("Cancel", null);
-		alert.show();
-	}
-
-	private void clearDataBeforeOneWeek() {
-		mDbHelper.deleteDataBeforeOneWeek();
-	}
-
-	private void clearLogBeforeOneWeek() {
-		mDbHelper.deleteLogBeforeOneWeek();
 	}
 }
--- trunk/src/iam/applications/PreferencesActivity.java
+++ trunk/src/iam/applications/PreferencesActivity.java
@@ -3,7 +3,21 @@
  */
 package iam.applications;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Date;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 
 /**
@@ -11,6 +25,9 @@ import android.preference.PreferenceActivity;
  * 
  */
 public class PreferencesActivity extends PreferenceActivity {
+
+	/** The database interface. */
+	private DbAdapter mDbHelper;
 
 	/*
 	 * (non-Javadoc)
@@ -20,7 +37,175 @@ public class PreferencesActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		mDbHelper = new DbAdapter(this);
+		mDbHelper.open();
+
 		addPreferencesFromResource(R.xml.preferences);
+
+		Preference emailDatabase = findPreference("email_database");
+		emailDatabase
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						emailDatabase();
+						return false;
+					}
+				});
+
+		Preference resetDatabase = findPreference("reset_database");
+		resetDatabase
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						resetDatabase();
+						return false;
+					}
+				});
+
+		Preference uploadDatabase = findPreference("upload_database");
+		uploadDatabase
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						uploadDatabase();
+						return false;
+					}
+				});
+
+		Preference clearOneWeekDatabase = findPreference("clear_oneweek");
+		clearOneWeekDatabase
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						mDbHelper.deleteDataBeforeOneWeek();
+						return false;
+					}
+				});
+		Preference clearLogDatabase = findPreference("clear_log");
+		clearLogDatabase
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						mDbHelper.deleteLogBeforeOneWeek();
+						return false;
+					}
+				});
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#finalize()
+	 */
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		mDbHelper.close();
+	}
+
+	private void emailDatabase() {
+		FileChannel source = null;
+		FileChannel destination = null;
+
+		final File sdcard = Environment.getExternalStorageDirectory();
+		File sourceFile = new File(mDbHelper.getPath());
+		File destFile = new File(sdcard, "temp/thedatabase");
+
+		try {
+			source = new FileInputStream(sourceFile).getChannel();
+			destination = new FileOutputStream(destFile).getChannel();
+			destination.transferFrom(source, 0, source.size());
+		} catch (Exception e) {
+			try {
+				e.printStackTrace();
+
+				if (source != null) {
+					source.close();
+				}
+				if (destination != null) {
+					destination.close();
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+		Intent sendIntent = new Intent(Intent.ACTION_SEND);
+		sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+		sendIntent.setType("application/x-sqlite3");
+		sendIntent.putExtra(Intent.EXTRA_SUBJECT, "24/7 App Database");
+		sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(destFile));
+		sendIntent.putExtra(Intent.EXTRA_TEXT,
+				"As of: " + Time.prettyDateTime(new Date()));
+		startActivity(Intent.createChooser(sendIntent, "Email:"));
+	}
+
+	private void uploadDatabase() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle(R.string.upload_database);
+		alert.setMessage(R.string.upload_database_instructions);
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				FileChannel source = null;
+				FileChannel destination = null;
+
+				final File sdcard = Environment.getExternalStorageDirectory();
+				File sourceFile = new File(sdcard, "temp/thedatabase");
+				File destFile = new File(mDbHelper.getPath());
+
+				try {
+					source = new FileInputStream(sourceFile).getChannel();
+					destination = new FileOutputStream(destFile).getChannel();
+					destination.transferFrom(source, 0, source.size());
+				} catch (Exception e) {
+					try {
+						e.printStackTrace();
+
+						if (source != null) {
+							source.close();
+						}
+						if (destination != null) {
+							destination.close();
+						}
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		alert.setNegativeButton("Cancel", null);
+		alert.show();
+	}
+
+	private void resetDatabase() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle(R.string.reset_database);
+		alert.setMessage(R.string.reset_database_first_warning);
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+
+				AlertDialog.Builder alert2 = new AlertDialog.Builder(
+						PreferencesActivity.this);
+				alert2.setTitle(R.string.reset_database);
+				alert2.setMessage(R.string.reset_database_second_warning);
+				alert2.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+
+								mDbHelper.deleteAll();
+							}
+						});
+
+				alert2.setNegativeButton("Cancel", null);
+				alert2.show();
+			}
+		});
+		alert.setNegativeButton("Cancel", null);
+		alert.show();
+	}
 }
